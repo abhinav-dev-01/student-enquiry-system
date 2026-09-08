e84611bb414369661d4b5e8a19deb9ee365771f7
Abhinav P <abhinavpadoli@gmail.com>
2026-09-08 14:52:31 +0530

    new updation

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -34,7 +34,7 @@ export default function HeroSection() {
   const videoScale = 1 + Math.min(scrollY * 0.00025, 0.08);
 
   return (
-    <section className="relative min-h-[58vh] sm:min-h-[65vh] md:min-h-[72vh] flex flex-col justify-center pt-20 pb-6 sm:pt-24 sm:pb-8 md:pt-28 md:pb-10 bg-white overflow-hidden">
+    <section className="relative min-h-[100dvh] h-[100dvh] sm:min-h-[75vh] sm:h-auto md:min-h-[85vh] flex flex-col justify-center pt-20 pb-16 sm:pt-24 sm:pb-12 md:pt-28 md:pb-16 bg-white overflow-hidden">
       {/* Background Video with Mobile-Responsive Framing & Smooth Scroll-Linked Zoom-in */}
       <div className="absolute inset-0 z-0 overflow-hidden pointer-events-none">
         <video
