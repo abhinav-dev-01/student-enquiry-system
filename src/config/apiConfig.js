f2474c822cc26a9e2d0fe4818ed7915b9cf38102
Abhinav P <abhinavpadoli@gmail.com>
2026-09-10 16:37:29 +0530

    new updation

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,1 +1,3 @@
-export const GOOGLE_APPS_SCRIPT_URL = import.meta.env.VITE_GOOGLE_APPS_SCRIPT_URL;
+export const GOOGLE_APPS_SCRIPT_URL = 
+  import.meta.env.VITE_GOOGLE_APPS_SCRIPT_URL || 
+  'https://script.google.com/macros/s/AKfycbzZEZ9dIXKfsnyjksbnDh8jcoOXCzqYSC_Lh21mYPOA2YEo-JvIG_0vVmqqYCXhuMmg3Q/exec';
